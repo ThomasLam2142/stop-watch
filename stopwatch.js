ff14b9198ca1f06da897828a11d68d1b56cf810f
ThomasLam2142 <thomaslam2142@gmail.com>
2023-08-24 15:39:05 -0400

    millisecond stuff implemented

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,8 @@
 let appState = "off";
 let currentTime = 0;
+let currentMillisec = 0;
 const spanElement = document.getElementById("displayTime");
+const milliSec = document.getElementById("displayMilli");
 
 
 
@@ -19,13 +21,15 @@ function stopClick(){
 function resetClick(){
     console.log("reset button clicked");
     currentTime = 0;
+    currentMillisec = 0;
     spanElement.textContent = currentTime.toString();
+    milliSec.textContent = currentMillisec.toString();
 }
 
 
 // Define your function
 function clockUpdate() {
-    console.log('This function will be called every 3 seconds.');
+    console.log('This function will be called every 1 seconds.');
     if (appState == "on"){
         console.log("appState is on");
         currentTime += 1;
@@ -35,9 +39,28 @@ function clockUpdate() {
 
     console.log(currentTime);
     spanElement.textContent = currentTime.toString();
+    
+}
+
+
+function clockUpdateMilli() {
+    console.log('This function will be called every 3 seconds.');
+    if (appState == "on"){
+        console.log("appState is on");
+        currentMillisec += 1;
+    }else{
+        console.log("appState is off");
+    }
+
+    if (currentMillisec>99){
+        currentMillisec = 0;
+    }
+
+    console.log(currentMillisec);
+    milliSec.textContent = currentMillisec.toString();
 
 }
   
 // Call the function every 1000 milli
 setInterval(clockUpdate, 1000);
-  
+setInterval(clockUpdateMilli, 10);
